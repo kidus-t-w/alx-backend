7e43caab8b1ef77727b38a8d0d438aae8a0d84ea
kidus-t-w <kidusk69@gmail.com>
2024-05-04 13:01:44 +0300

    Fixed TAsk 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """0-simple_helper_function.py"""
 
+from typing import Tuple
 
-def index_range(page: int, page_size:int) -> tuple:
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    '''Returns tuple of integers
+        Args:
+            page (int): page number.
+            page_size (int): number of data in a page.
+    '''
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)

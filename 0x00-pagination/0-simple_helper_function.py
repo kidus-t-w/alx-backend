02cbf18325e5b355ab2a2c40dfd7ccbefd7a05e5
kidus-t-w <kidusk69@gmail.com>
2024-05-04 12:57:22 +0300

    Fixed TAsk 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 """0-simple_helper_function.py"""
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size:int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)

7484d20998f23c8311df5834627b60f51e2e146a
kidus-t-w <kidusk69@gmail.com>
2024-05-08 14:25:01 +0300

    Added get and put method for BasicCasche class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Basic caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basic caching system"""
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            pass
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if (key is None):
+            return None
+        self.cache_data[key] = item

d27e718d322d99c8efe9c5784a143b4c658bf340
kidus-t-w <kidusk69@gmail.com>
2024-05-08 23:13:46 +0300

    Fixed get method

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -18,19 +18,30 @@ class MRUCache(BaseCaching):
         """
         Add an item to the cache.
         """
+        def __init__(self):
+            """Initializes the cache.
+            """
+            super().__init__()
+            self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
-        if key not in self.cache_data and item is not None:
-            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
-                removed, _ = self.cache_data.popitem(True)
-                print("DESTROYED:", removed)
-        self.cache_data[key] = item
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """
         Get an item form the cache by a key
         """
-        if key in self.cache_data:
+        if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
-            return self.cache_data.get(key, None)
-        return None
+        return self.cache_data.get(key, None)

d25572292f1f54e33743bbaceeb7cceaa2b24ecc
kidus-t-w <kidusk69@gmail.com>
2024-05-08 14:27:26 +0300

    Made code strongly typed

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,17 +8,28 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """ Basic caching system"""
-    def get(self, key):
+    def get(self, key: str) -> Optional[Any]:
         """ Get an item by key
-        """
-        if key in self.cache_data:
-            return self.cache_data[key]
-        else:
-            pass
 
-    def put(self, key, item):
-        """ Add an item in the cache
+        Args:
+            key (str): key of the item to retrieve
+
+        Returns:
+            Optional[Any]: item retrieved from the
+              cache or None if not found
         """
-        if (key is None):
+        return self.cache_data.get(key)
+
+    def put(self, key: str, item: Any) -> None:
+        """ Add an item in the cache
+
+        Args:
+            key (str): key of the item to add
+            item (Any): item to add
+
+        Returns:
+            None
+        """
+        if key is None:
             return None
         self.cache_data[key] = item

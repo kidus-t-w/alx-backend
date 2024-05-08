4b469b452971a2b53fd5df4542858b2bb738003a
kidus-t-w <kidusk69@gmail.com>
2024-05-08 14:33:02 +0300

    Added condition for put method

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -19,6 +19,5 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if (key is None):
-            return None
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item

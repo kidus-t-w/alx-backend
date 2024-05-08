cd64c17f7d0907df198773128e9fa7d2ff496897
kidus-t-w <kidusk69@gmail.com>
2024-05-08 14:31:19 +0300

    Added doc string to get method

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,28 +8,17 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """ Basic caching system"""
-    def get(self, key: str) -> Optional[Any]:
+    def get(self, key):
         """ Get an item by key
-
-        Args:
-            key (str): key of the item to retrieve
-
-        Returns:
-            Optional[Any]: item retrieved from the
-              cache or None if not found
         """
-        return self.cache_data.get(key)
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            pass
 
-    def put(self, key: str, item: Any) -> None:
+    def put(self, key, item):
         """ Add an item in the cache
-
-        Args:
-            key (str): key of the item to add
-            item (Any): item to add
-
-        Returns:
-            None
         """
-        if key is None:
+        if (key is None):
             return None
         self.cache_data[key] = item

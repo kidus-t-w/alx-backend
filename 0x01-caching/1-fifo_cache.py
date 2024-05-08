8af514d77151c33cd2e9736bf00c19605a3f1916
kidus-t-w <kidusk69@gmail.com>
2024-05-08 16:32:26 +0300

    Modified class docstirng

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,10 +7,6 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with a FIFO
-    removal mechanism when the limit is reached.
-    """
     def __init__(self):
         """Initializes the cache.
         """
@@ -19,13 +15,22 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Adds an item in the cache.
+
+        If the key or item is None, this method does nothing.
+
+        If the cache is full, the oldest item is removed from the cache
+        and a message is printed indicating which key was removed.
+
+        Args:
+            key (object): The key used to store the item.
+            item (object): The item to store in the cache.
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD:", first_key)
+            print("DISCARD:", first_key)  # noqa: T001
 
     def get(self, key):
         """Retrieves an item by key.

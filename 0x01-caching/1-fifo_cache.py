7a8bc0ad47914847b11bdc9d3fcbf6bb0842eef4
kidus-t-w <kidusk69@gmail.com>
2024-05-08 16:07:46 +0300

    Modified put method

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,44 +1,4 @@
 #!/usr/bin/env python3
-"""
-FIFO cache system
-"""
-
-from collections import OrderedDict
-BaseCaching = __import__('base_caching').BaseCaching
-
-
-class FIFOCache(BaseCaching):
-    """
-    FIFOCache system
-    """
-    def __init__(self):
-        """Initializes the cache.
-        """
-        super().__init__()
-        self.cache_data = OrderedDict()
-
-    def put(self, key, item):
-        """
-        Add item in the cache
-        """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
-
-        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
-            first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD:{}".format(first_key))
-
-    def get(self, key):
-        """
-        Return item form the cache
-        """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
-
-
-#!/usr/bin/env python3
 """First-In First-Out caching module.
 """
 from collections import OrderedDict

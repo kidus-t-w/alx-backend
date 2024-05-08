c488fdffbf6e69e42ec9f86ff3cb2aa8a1971744
kidus-t-w <kidusk69@gmail.com>
2024-05-08 16:03:10 +0300

    Modified put method

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
+"""First-In First-Out caching module.
 """
-FIFO cache system
-"""
-
 from collections import OrderedDict
-BaseCaching = __import__('base_caching').BaseCaching
+
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-    FIFOCache system
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
     """
     def __init__(self):
         """Initializes the cache.
@@ -18,21 +18,16 @@ class FIFOCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """
-        Add item in the cache
+        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
-
-        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD:{}".format(first_key))
+            print("DISCARD:", first_key)
 
     def get(self, key):
+        """Retrieves an item by key.
         """
-        Return item form the cache
-        """
-        if key is None and key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)

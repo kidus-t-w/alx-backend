8579d0194af4b8922ae43f6bbc93701dd69543d6
kidus-t-w <kidusk69@gmail.com>
2024-05-14 16:29:12 +0300

    Formatted file

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+"""
+A simple flask app
+"""
 from flask import Flask, render_template
 from flask_babel import Babel
+
 
 class Config:
     """Config class for Flask app
@@ -12,9 +16,11 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @app.route('/')
 def home():
@@ -25,6 +31,7 @@ def home():
         HTML: The rendered index page.
     """
     return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)

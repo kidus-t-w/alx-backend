fd952d5db9b40fad57257ce6af439d998e44490b
kidus-t-w <kidusk69@gmail.com>
2024-05-14 16:25:43 +0300

    Make change

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+"""
+Simple app
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def index():
     """
     Renders the index page.
 

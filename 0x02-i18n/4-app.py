d6fe55016093bfe2e3f2679511eec1a889fcf460
kidus-t-w <kidusk69@gmail.com>
2024-05-14 17:05:30 +0300

    Modified 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -27,11 +27,11 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """returns a user dictionary or None if the ID cannot be found
+    """
+    Get the locale for the request
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
-        print(locale)
         return locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
@@ -42,12 +42,13 @@ def index():
     """
     Executes before each request.
 
-    Retrieves the user associated with the request and stores it in the global context.
+    Retrieves the user associated with the request and
+    stores it in the global context.
 
     Returns:
         None
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':

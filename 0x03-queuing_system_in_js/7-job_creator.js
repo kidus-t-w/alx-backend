076992440c9c564ea9ec6e28be687861b81af7a8
kidus-t-w <kidusk69@gmail.com>
2024-06-12 21:24:19 +0300

    Modified task

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -19,38 +19,6 @@ const jobs = [
     phoneNumber: '4153518743',
     message: 'This is the code 4321 to verify your account',
   },
-  {
-    phoneNumber: '4153538781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4153118782',
-    message: 'This is the code 4321 to verify your account',
-  },
-  {
-    phoneNumber: '4153718781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4159518782',
-    message: 'This is the code 4321 to verify your account',
-  },
-  {
-    phoneNumber: '4158718781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4153818782',
-    message: 'This is the code 4321 to verify your account',
-  },
-  {
-    phoneNumber: '4154318781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4151218782',
-    message: 'This is the code 4321 to verify your account',
-  },
 ];
 
 for (let job of jobs) {
